d8da99b2031474410a7cb497a07272b6274202ca
mimipeshy <perisndanu@gmail.com>
2021-06-04 18:32:45 +0300

    fix rubocop errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
   has_many :expenses
-  validates_presence_of :username
-#   validates :username, presence: true, uniqueness: true, length: { minimum: 4,
-                                                                #    too_short: 'username can be minimum of 4 characters long.' }
+  validates :username, presence: true, uniqueness: true, length: { minimum: 4,
+                                                                   too_short: 'username can be minimum of 4 characters long.' }
 end
