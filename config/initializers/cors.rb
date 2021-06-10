f212efc30a81b4f2e27e65839d1e55ee17987e03
mimipeshy <perisndanu@gmail.com>
2021-06-10 23:23:02 +0300

    Fix cors origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,6 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
+    origins 'peris-expense-tracker.netlify.app'
     origins 'http://localhost:3000'
 
     resource '*',
