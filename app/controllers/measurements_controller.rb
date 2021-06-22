27ec2e845750673a453a862ad8d250f131babe6b
mimipeshy <perisndanu@gmail.com>
2021-06-22 19:54:55 +0300

    Fix rubocop errors

--- app/controllers/measurements_controller.rb
+++ app/controllers/measurements_controller.rb
@@ -1,52 +1,52 @@
 class MeasurementsController < ApplicationController
-    before_action :set_measurement, only: %i[show update destroy]
+  before_action :set_measurement, only: %i[show update destroy]
 
-    # GET /measurements
-    def index
-      @measurements = Measurement.all
-  
-      render json: @measurements
+  # GET /measurements
+  def index
+    @measurements = Measurement.all
+
+    render json: @measurements
+  end
+
+  # GET /measurements/1
+  def show
+    render json: @measurement
+  end
+
+  # POST /measurements
+  def create
+    @measurement = Measurement.new(measurement_params)
+
+    if @measurement.save
+      render json: @measurement, status: :created, location: @measurement
+    else
+      render json: @measurement.errors, status: :unprocessable_entity
     end
-  
-    # GET /measurements/1
-    def show
+  end
+
+  # PATCH/PUT /measurements/1
+  def update
+    if @measurement.update(measurement_params)
       render json: @measurement
+    else
+      render json: @measurement.errors, status: :unprocessable_entity
     end
-  
-    # POST /measurements
-    def create
-      @measurement = Measurement.new(measurement_params)
-  
-      if @measurement.save
-        render json: @measurement, status: :created, location: @measurement
-      else
-        render json: @measurement.errors, status: :unprocessable_entity
-      end
-    end
-  
-    # PATCH/PUT /measurements/1
-    def update
-      if @measurement.update(measurement_params)
-        render json: @measurement
-      else
-        render json: @measurement.errors, status: :unprocessable_entity
-      end
-    end
-  
-    # DELETE /measurements/1
-    def destroy
-      @measurement.destroy
-    end
-  
-    private
-  
-    # Use callbacks to share common setup or constraints between actions.
-    def set_measurement
-      @measurement = Measurement.find(params[:id])
-    end
-  
-    # Only allow a list of trusted parameters through.
-    def measurement_params
-      params.permit(:amount, :date, :expense_id)
-    end
+  end
+
+  # DELETE /measurements/1
+  def destroy
+    @measurement.destroy
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_measurement
+    @measurement = Measurement.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def measurement_params
+    params.permit(:amount, :date, :expense_id)
+  end
 end
