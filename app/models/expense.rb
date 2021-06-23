ee346e9a6758ab366f6d6101c8cc67d39f5d9d61
mimipeshy <perisndanu@gmail.com>
2021-06-24 00:41:01 +0300

    Fix rubocop errors

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -2,5 +2,5 @@ class Expense < ApplicationRecord
   belongs_to :user
   has_many :measurements, dependent: :destroy
   # validations
-  validates_presence_of :name, unique:true
+  validates_presence_of :name, unique: true
 end
