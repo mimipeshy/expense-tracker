a82e7640af6a9d80f5e005237048c8bc86d20a4b
mimipeshy <perisndanu@gmail.com>
2021-06-28 15:18:36 +0300

    Update CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'https://peris-expense-track.herokuapp.com'
+      origins '*'
       resource '*', headers: :any, methods: [:get, :post, :patch, :put]
     end
   end
