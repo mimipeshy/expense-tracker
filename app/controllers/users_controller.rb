769d09cdf101a9a6bb05bbc6e346397e8c39bd1a
mimipeshy <perisndanu@gmail.com>
2021-06-30 23:33:02 +0300

    Solve routing error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,30 @@
 class UsersController < ApplicationController
-  skip_before_action :authorize_request, only: :create
+  # skip_before_action :authorize_request, only: :create
   # POST /signup
   # return authenticated token upon signup
+  before_action :set_user, only: [:show, :update, :destroy]
+
+  # GET /users
+  def index
+    @users = User.all
+
+    render json: @users
+  end
+
   def create
-    user = User.create!(user_params)
-    auth_token = AuthenticateUser.new(user.email, user.password).call
-    response = { message: Message.account_created, auth_token: auth_token }
-    json_response(response, :created)
+    @user = User.new(user_params)
+
+    if @user.save
+      render json: @user, status: :created, location: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
   end
 
   private
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.permit(
