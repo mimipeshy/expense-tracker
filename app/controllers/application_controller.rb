6c730efdbb7cf924f2867488c34d1a0de89431be
mimipeshy <perisndanu@gmail.com>
2021-06-30 23:18:32 +0300

    Remove auth tokens

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::API
   include ExceptionHandler
 
   # called before every action on controllers
-  before_action :authorize_request
+  # before_action :authorize_request
   attr_reader :current_user
 
   private
