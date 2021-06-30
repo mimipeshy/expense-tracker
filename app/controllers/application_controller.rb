a51742dc3681684c49ef0c6f9f710fe028f1e04e
mimipeshy <perisndanu@gmail.com>
2021-07-01 00:03:58 +0300

    update user controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,12 +4,12 @@ class ApplicationController < ActionController::API
 
   # called before every action on controllers
   # before_action :authorize_request
-  attr_reader :current_user
+  # attr_reader :current_user
 
-  private
+  # private
 
-  # Check for valid request token and return user
-  def authorize_request
-    @current_user = (AuthorizeApiRequest.new(request.headers).call)[:user]
-  end
+  # # Check for valid request token and return user
+  # def authorize_request
+  #   @current_user = (AuthorizeApiRequest.new(request.headers).call)[:user]
+  # end
 end
