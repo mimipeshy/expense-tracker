1b2cf72ea9bf699afff1a6f43609f80750179e5f
mimipeshy <perisndanu@gmail.com>
2021-07-01 23:26:59 +0300

    Resolve auth error

--- app/auth/authorize_api_request.rb
+++ app/auth/authorize_api_request.rb
@@ -1,41 +1,41 @@
-# class AuthorizeApiRequest
-#   def initialize(headers = {})
-#     @headers = headers
-#   end
+class AuthorizeApiRequest
+  def initialize(headers = {})
+    @headers = headers
+  end
 
-#   # Service entry point - return valid user object
-#   def call
-#     {
-#       user: user
-#     }
-#   end
+  # Service entry point - return valid user object
+  def call
+    {
+      user: user
+    }
+  end
 
-#   private
+  private
 
-#   attr_reader :headers
+  attr_reader :headers
 
-#   def user
-#     # check if user is in the database
-#     # memoize user object
-#     @user ||= User.find(decoded_auth_token[:user_id]) if decoded_auth_token
-#     # handle user not found
-#   rescue ActiveRecord::RecordNotFound => e
-#     # raise custom error
-#     raise(
-#       ExceptionHandler::InvalidToken,
-#       ("#{Message.invalid_token} #{e.message}")
-#     )
-#   end
+  def user
+    # check if user is in the database
+    # memoize user object
+    @user ||= User.find(decoded_auth_token[:user_id]) if decoded_auth_token
+    # handle user not found
+  rescue ActiveRecord::RecordNotFound => e
+    # raise custom error
+    raise(
+      ExceptionHandler::InvalidToken,
+      ("#{Message.invalid_token} #{e.message}")
+    )
+  end
 
-#   # decode authentication token
-#   def decoded_auth_token
-#     @decoded_auth_token ||= JsonWebToken.decode(http_auth_header)
-#   end
+  # decode authentication token
+  def decoded_auth_token
+    @decoded_auth_token ||= JsonWebToken.decode(http_auth_header)
+  end
 
-#   # check for token in `Authorization` header
-#   def http_auth_header
-#     return headers['Authorization'].split(' ').last if headers['Authorization'].present?
+  # check for token in `Authorization` header
+  def http_auth_header
+    return headers['Authorization'].split(' ').last if headers['Authorization'].present?
 
-#     raise(ExceptionHandler::MissingToken, Message.missing_token)
-#   end
-# end
+    raise(ExceptionHandler::MissingToken, Message.missing_token)
+  end
+end
